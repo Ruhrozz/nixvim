79a6afc480a9b664112fc0ea388a9789fcee14b0
Ruhrozz <ruhrozz@bk.ru>
2024-08-01 23:52:59 +0700

    fix: treesitter

--- config/plugins/treesitter.nix
+++ config/plugins/treesitter.nix
@@ -3,28 +3,14 @@
     enable = true;
 
     settings = {
-      auto_install = false;
+      auto_install = true;
       ensure_installed = "all";
       highlight = {
         additional_vim_regex_highlighting = true;
         custom_captures = { };
-        disable = [ "rust" ];
         enable = true;
-      };
-      ignore_install = [ "rust" ];
-      incremental_selection = {
-        enable = true;
-        keymaps = {
-          init_selection = false;
-          node_decremental = "grm";
-          node_incremental = "grn";
-          scope_incremental = "grc";
-        };
       };
       indent = { enable = true; };
-      parser_install_dir = {
-        __raw = "vim.fs.joinpath(vim.fn.stdpath('data'), 'treesitter')";
-      };
       sync_install = false;
     };
   };
